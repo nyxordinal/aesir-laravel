6a1bd735bac029e7ab83c3945aa6d7cb98d46a4d
Muhammad Fakhri <muhammad_fakhri@apps.ipb.ac.id>
2020-01-24 17:57:49 +0700

    Create a migration for Main table along with the model

---
+++ database/migrations/2020_01_24_091050_create_mains_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mains', function (Blueprint $table) {
+            $table->bigIncrements('ain');
+            $table->string('title', 255);
+            $table->text('genre')->nullable();
+            $table->smallInteger('episode')->nullable();
+            $table->dateTime('airing_from')->nullable();
+            $table->dateTime('airing_until')->nullable();
+            $table->tinyInteger('type')->default(0);
+            $table->tinyInteger('status')->default(0);
+            $table->tinyInteger('download_status')->default(0);
+            $table->tinyInteger('resolution')->nullable()->default(0);
+            $table->tinyInteger('storage_device')->nullable()->default(0);
+            $table->text('note')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mains');
+    }
+}
