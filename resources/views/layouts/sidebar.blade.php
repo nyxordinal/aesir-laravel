1dea1ba02b41a46c19a9b9edbd4e1cb937b0891e
Muhammad Fakhri <muhammad_fakhri@apps.ipb.ac.id>
2020-01-24 17:50:51 +0700

    Add home url and change sidebar title to Aesir

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -3,7 +3,7 @@
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
         <img src="{{ asset('dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
+        <span class="brand-text font-weight-light">Aesir</span>
     </a>
     <!-- Sidebar -->
     <div class="sidebar">
@@ -22,7 +22,7 @@
                 <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
                 <li class="nav-item has-treeview">
-                    <a href="#" class="nav-link">
+                    <a href="/" class="nav-link">
                         <i class="nav-icon fas fa-home"></i>
                         <p>
                             Home
