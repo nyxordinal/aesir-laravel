ab241dec5dba404aa17eedcb053bf65d583bc6b5
Muhammad Fakhri <muhammadfakhri301@gmail.com>
2020-03-16 15:14:48 +0700

    Add auth middleware to every route

--- routes/web.php
+++ routes/web.php
@@ -12,25 +12,26 @@
  */
 
 Auth::routes([
-	'register' => false,
-	'reset' => false,
-	'verify' => false,
+    'register' => false,
+    'reset' => false,
+    'verify' => false,
 ]);
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/', 'HomeController@index')->name('home');
 
-Route::get('/', 'HomeController@index')->name('home');
+    Route::get('/detail/{id}', 'EditController@detail');
+    Route::post('/detail/{id}', 'EditController@update');
 
-Route::get('/detail/{id}', 'EditController@detail');
-Route::post('/detail/{id}', 'EditController@update');
+    Route::get('/create', 'AddController@display')->name('insert');
+    Route::post('/create', 'AddController@insertNewAnime');
 
-Route::get('/create', 'AddController@display')->name('insert');
-Route::post('/create', 'AddController@insertNewAnime');
-
-// Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/watching', 'HomeController@watching')->name('watching');
-Route::get('/watched', 'HomeController@watched')->name('watched');
-Route::get('/plan', 'HomeController@plan')->name('plan');
-Route::get('/hold', 'HomeController@hold')->name('hold');
-Route::get('/drop', 'HomeController@drop')->name('drop');
-Route::get('/no', 'HomeController@no')->name('no');
-Route::get('/complete', 'HomeController@complete')->name('complete');
-Route::get('/process', 'HomeController@process')->name('process');
+    // Route::get('/home', 'HomeController@index')->name('home');
+    Route::get('/watching', 'HomeController@watching')->name('watching');
+    Route::get('/watched', 'HomeController@watched')->name('watched');
+    Route::get('/plan', 'HomeController@plan')->name('plan');
+    Route::get('/hold', 'HomeController@hold')->name('hold');
+    Route::get('/drop', 'HomeController@drop')->name('drop');
+    Route::get('/no', 'HomeController@no')->name('no');
+    Route::get('/complete', 'HomeController@complete')->name('complete');
+    Route::get('/process', 'HomeController@process')->name('process');
+});
