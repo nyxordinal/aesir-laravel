2b36a36a7b6ec6325ce01981e2dd351ad9eea7ba
Muhammad Fakhri <muhammadfakhri301@gmail.com>
2020-03-16 14:33:41 +0700

    Change some detail in user seeder

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Carbon;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->delete();
+        User::create([
+            'name' => 'Muhammad Fakhri',
+            'email' => 'fakhri@codepanda.id',
+            'password' => Hash::make('password'),
+            'email_verified_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+    }
+}
